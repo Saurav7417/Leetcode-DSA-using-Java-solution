eae57ad50b32cd29726cd10f168e7ca3730b7b51
Saurav@Kumar <119718360+Saurav7417@users.noreply.github.com>
2025-02-08 19:59:23 +0530

    Time: 1 ms (84.14%), Space: 42.2 MB (38.71%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,17 +1,32 @@
+// class Solution {
+//     public List<List<Integer>> generate(int n) {
+//         List<List<Integer>> result = new ArrayList<>();
+//         for (int i = 0; i < n; i++) {
+//             List<Integer> row = new ArrayList<>();
+//             row.add(1);
+//             for (int j = 1; j < i; j++) {
+//                 row.add(result.get(i - 1).get(j - 1) + result.get(i - 1).get(j));
+//             }
+//             if (i > 0) row.add(1);
+//             result.add(row);
+//         }
+//         return result;        
+//     }
+// }
+
+
 class Solution {
     public List<List<Integer>> generate(int n) {
         List<List<Integer>> result = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+        for(int i=0; i<n; i++){
             List<Integer> row = new ArrayList<>();
             row.add(1);
-            for (int j = 1; j < i; j++) {
-                row.add(result.get(i - 1).get(j - 1) + result.get(i - 1).get(j));
+            for(int j=1; j<i; j++){
+                row.add(result.get(i-1).get(j-1) + result.get(i-1).get(j));
             }
-            if (i > 0) row.add(1);
+            if(i>0) row.add(1);
             result.add(row);
         }
         return result;
-        
-    }
 }
-
+}
